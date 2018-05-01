8b2b9d021b5321a5daaf2f9d4fedb7f689f3e71b
Murat Celik <cashkl@yandex.com>
2018-05-01 16:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(arr,str) {
   arr.push(str);
   return `Welcome, ${str}. You are number ${arr.length} in line.`
 }
+function nowServing(arr) {
+
+  if (arr.length==0) {
+    return"There is nobody waiting to be served!""
+  }
+}
